2569a747e0e095aafb1dbb3a8eb94fdfe06582aa
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 15:51:10 +0900

    배열 및 반복문을 이용하여 띠 출력하기

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,19 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+
+            string[] cz = { "원숭이", "닭", "개", "돼지", "쥐", "소", "호랑이", "토끼", "용", "뱀", "말", "양" };
+
+            Console.Write("태어난 년도 입력 : ");
+            int year = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<12; i++)
+            {
+                if (year % 12 == i)
+                {
+                    Console.WriteLine(cz[i]); break;
+                }
+            }
         }
     }
 }
